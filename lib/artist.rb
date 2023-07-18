5f03ff6ec1755f7c8fda2333144cb183e2d2f7bb
Antonio <antonio18casta@aol.com>
2023-07-18 16:20:51 -0400

    commit tree 502db4e0d3ddbbb0334af218171ca226b26d7034

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,26 @@
 require 'pry'
-
+# The extend keyword is responsible for defining the method as a class method vs.
+# an instance method (which would use the include keyword).
 class Artist
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
+
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
+  # The super keyword, placed inside a method, will tell that method to look up its behavior in the
+  # method of the same name that lives in the parent, or super, class.
   def initialize
-    @@artists << self
+    super
     @songs = []
-  end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
   end
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
@@ -34,9 +30,5 @@ class Artist
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 end
